c9c248c6bf3336d01dbd49a091017eeedb4c3da5
dew <djajaatm@seattleu.edu>
2018-04-29 02:00:21 -0700

    hw2 prep files

--- hw2data.js
+++ hw2data.js
@@ -9,12 +9,12 @@
       "address": "8070 Melrose Ave"
     },
     {
-      "name": "James Bond",
-      "email": "newemail@email.com",
-      "phone": "202-555-0183",
-      "gender": "male",
-      "birthdate": "07/24/84",
-      "address": "8070 Melrose Ave"
+      "name": "Brielle Williamson",
+      "email": "briew@rocketmail.com",
+      "phone": "202-545-0265",
+      "gender": "female",
+      "birthdate": "03/02/94",
+      "address": "451 S Hewitt St"
     }
   ]
 }
