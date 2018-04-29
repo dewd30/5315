f76a7a75e145008138a8d3d254087ea79483a70a
dew <djajaatm@seattleu.edu>
2018-04-29 03:26:55 -0700

    hw2 prep files

--- hw2data.js
+++ hw2data.js
@@ -17,12 +17,36 @@
       "address": "451 S Hewitt St"
     },
     {
-      "name": "Brielle Williamson",
-      "email": "briew@rocketmail.com",
-      "phone": "202-545-0265",
+      "name": "Jordan Dunn",
+      "email": "jdunn@gmail.com",
+      "phone": "617-585-0151",
+      "gender": "male",
+      "birthdate": "08/14/90",
+      "address": "145 Dartmouth St"
+    },
+    {
+      "name": "Kara Seymour",
+      "email": "karads@mail.com",
+      "phone": "206-505-0151",
       "gender": "female",
-      "birthdate": "03/02/94",
-      "address": "451 S Hewitt St"
-    }
+      "birthdate": "05/19/92",
+      "address": "145 5th Ave"
+    },
+    {
+      "name": "Clarissa Clarkee",
+      "email": "cl@cla.com",
+      "phone": "202-355-7014",
+      "gender": "female",
+      "birthdate": "07/24/84",
+      "address": "807 Causeway St"
+    },
+    {
+      "name": "Aidan Berg",
+      "email": "aberg@gmail.com",
+      "phone": "2066-347-5129",
+      "gender": "male",
+      "birthdate": "11/28/85",
+      "address": "457 108th St"
+    },
   ]
 }
