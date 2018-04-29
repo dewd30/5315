d95ac591ad8e92c2d04419a3520778751356ad2f
dew <djajaatm@seattleu.edu>
2018-04-29 01:01:52 -0700

    hw2 prep files

--- hw2data.js
+++ hw2data.js
@@ -2,11 +2,19 @@
   "data": [
     {
       "name": "James Bond",
-      "email": "newemail@email.com"
+      "email": "newemail@email.com",
+      "phone": "202-555-0183",
+      "gender": "male",
+      "birthdate": "07/24/84",
+      "address": "8070 Melrose Ave"
     },
     {
       "name": "Brielle Williamson",
-      "email": "jbond@su.edu"
+      "email": "briew@rocketmail.com"
+      "phone": "202-545-0265",
+      "gender": "female",
+      "birthdate": "03/02/94",
+      "address": "451 S Hewitt St"
     }
   ]
 }
