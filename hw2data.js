2bf213e8c85c3aaeaceb283679fc7ba355023674
dew <djajaatm@seattleu.edu>
2018-04-29 23:04:18 -0700

    hw2 prep files

--- hw2data.js
+++ hw2data.js
@@ -13,7 +13,7 @@
       "email": "briew@rocketmail.com",
       "phone": "202-545-0265",
       "gender": "female",
-      "birthdate": "03/02/94",
+      "birthdate": "03/02/74",
       "address": "451 S Hewitt St"
     },
     {
@@ -41,12 +41,84 @@
       "address": "807 Causeway St"
     },
     {
-      "name": "Aidan Berg",
+      "name": "Aidan Bergman",
       "email": "aberg@gmail.com",
-      "phone": "2066-347-5129",
+      "phone": "206-347-5129",
       "gender": "male",
       "birthdate": "11/28/85",
       "address": "457 108th St"
+    },
+    {
+      "name": "Susan White",
+      "email": "swhite@yahoo.com",
+      "phone": "202-878-0113",
+      "gender": "female",
+      "birthdate": "01/28/97",
+      "address": "403 S 57th St"
+    },
+    {
+      "name": "Roger Ashen",
+      "email": "rogerash@hotmail.com",
+      "phone": "206-872-0173",
+      "gender": "male",
+      "birthdate": "04/13/77",
+      "address": "426 Longfellow Bridge"
+    },
+    {
+      "name": "Kiara Lee",
+      "email": "kikilee@outlook.com",
+      "phone": "202-878-0113",
+      "gender": "female",
+      "birthdate": "07/18/89",
+      "address": "200 Park Avenue"
+    },
+    {
+      "name": "Tanaka Reo",
+      "email": "treo@gmail.com",
+      "phone": "212-535-9555",
+      "gender": "male",
+      "birthdate": "09/01/69",
+      "address": "709 Honey Creek Dr"
+    },
+    {
+      "name": "Florence Garcon",
+      "email": "flogarcon79@treeo.com",
+      "phone": "347-622-2329",
+      "gender": "female",
+      "birthdate": "02/26/79",
+      "address": "73 Pacific St"
+    },
+    {
+      "name": "Robert Hernandez",
+      "email": "robbyh@pronto.com",
+      "phone": "206-622-2388",
+      "gender": "male",
+      "birthdate": "05/26/93",
+      "address": "812 Thatcher Court"
+    },
+    {
+      "name": "Ailee Stark",
+      "email": "astark35@rocketmail.com",
+      "phone": "206-792-7979",
+      "gender": "female",
+      "birthdate": "03/05/90",
+      "address": "3 S Sherman St"
+    },
+    {
+      "name": "David Archer",
+      "email": "david1988@gmail.com",
+      "phone": "206-792-7979",
+      "gender": "male",
+      "birthdate": "03/05/88",
+      "address": "3 S Sherman St"
+    },
+    {
+      "name": "Omar Joseph",
+      "email": "omareeo@yahoo.com",
+      "phone": "206-788-3888",
+      "gender": "male",
+      "birthdate": "12/27/85",
+      "address": "93 Bayport Ave"
     }
   ]
 }
