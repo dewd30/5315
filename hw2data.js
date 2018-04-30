392c3ce7bdb5c8b7e14fceee5c1182e2b485bd43
dew <djajaatm@seattleu.edu>
2018-04-30 01:46:01 -0700

    hw2 prep files

--- hw2data.js
+++ hw2data.js
@@ -3,18 +3,18 @@
     {
       "name": "Ian Bourdin",
       "email": "ianb7@gmail.com",
-      "phone": "202-555-0183",
+      "phone": "213-555-0183",
       "gender": "male",
       "birthdate": "07/24/84",
-      "address": "8070 Melrose Ave"
+      "address": "8070 Melrose Ave, Los Angeles, CA"
     },
     {
       "name": "Brie Williams",
       "email": "briew@rocketmail.com",
-      "phone": "202-545-0265",
+      "phone": "213-545-0265",
       "gender": "female",
       "birthdate": "03/02/74",
-      "address": "451 S Hewitt St"
+      "address": "451 S Hewitt St, Los Angeles, CA"
     },
     {
       "name": "Jordan Dunn",
@@ -22,7 +22,7 @@
       "phone": "617-585-0151",
       "gender": "female",
       "birthdate": "08/14/90",
-      "address": "145 Dartmouth St"
+      "address": "145 Dartmouth St, Boston, MA"
     },
     {
       "name": "Kara Seymour",
@@ -30,15 +30,15 @@
       "phone": "206-505-0151",
       "gender": "female",
       "birthdate": "05/19/92",
-      "address": "387 5th Ave"
+      "address": "387 5th Ave, Seattle, WA"
     },
     {
       "name": "Clarissa Clarkee",
       "email": "cl@cla.com",
-      "phone": "202-355-7014",
+      "phone": "857-355-7014",
       "gender": "female",
       "birthdate": "07/24/84",
-      "address": "807 Causeway St"
+      "address": "807 Causeway St, Boston, MA"
     },
     {
       "name": "Aidan Bergman",
@@ -46,31 +46,31 @@
       "phone": "206-347-5129",
       "gender": "male",
       "birthdate": "11/28/85",
-      "address": "457 108th St"
+      "address": "457 108th St SW, Everett, WA"
     },
     {
       "name": "Susan White",
       "email": "swhite@yahoo.com",
-      "phone": "202-878-0113",
+      "phone": "253-878-0113",
       "gender": "female",
       "birthdate": "01/28/67",
-      "address": "403 S 57th St"
+      "address": "403 S 57th St, Tacoma, WA"
     },
     {
       "name": "Roger Ashen",
       "email": "rogerash@hotmail.com",
-      "phone": "206-872-0173",
+      "phone": "202-872-0173",
       "gender": "male",
       "birthdate": "04/13/77",
-      "address": "426 Longfellow Bridge"
+      "address": "426 Longfellow St NW, Washington, DC"
     },
     {
       "name": "Kiara Lee",
       "email": "kikilee@outlook.com",
-      "phone": "202-878-0113",
+      "phone": "646-878-0113",
       "gender": "female",
       "birthdate": "07/18/89",
-      "address": "200 Park Avenue"
+      "address": "200 Park Ave, New York, NY"
     },
     {
       "name": "Tanaka Reo",
@@ -78,7 +78,7 @@
       "phone": "212-535-9555",
       "gender": "male",
       "birthdate": "09/01/69",
-      "address": "709 Honey Creek Dr"
+      "address": "709 Honey Creek Dr, New York, NY"
     },
     {
       "name": "Florence Garcon",
@@ -86,39 +86,39 @@
       "phone": "347-622-2329",
       "gender": "female",
       "birthdate": "02/26/79",
-      "address": "73 Pacific St"
+      "address": "73 Pacific St, Forest Hills, NY"
     },
     {
       "name": "Robert Hernandez",
       "email": "robbyh@pronto.com",
-      "phone": "206-622-2388",
+      "phone": "914-622-2388",
       "gender": "male",
       "birthdate": "05/26/93",
-      "address": "812 Thatcher Court"
+      "address": "812 Thatcher Court, Yonkers, NY"
     },
     {
       "name": "Ailee Stark",
       "email": "astark35@rocketmail.com",
-      "phone": "206-792-7979",
+      "phone": "503-792-7979",
       "gender": "female",
       "birthdate": "03/05/90",
-      "address": "3 S Sherman St"
+      "address": "3 S Sherman St, Astoria, NY"
     },
     {
       "name": "David Archer",
       "email": "david1988@gmail.com",
-      "phone": "206-792-7979",
+      "phone": "214-205-1871",
       "gender": "male",
       "birthdate": "03/05/88",
-      "address": "3 S Sherman St"
+      "address": "1109 Poco Mas Dr, Dallas, TX"
     },
     {
       "name": "Omar Joseph",
       "email": "omareeo@yahoo.com",
-      "phone": "206-788-3888",
+      "phone": "512-619-9655",
       "gender": "male",
       "birthdate": "12/27/85",
-      "address": "93 Bayport Ave"
+      "address": "3588 Station Street, Austin, TX"
     }
   ]
 }
